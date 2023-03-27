43f08e5524c37c23d4a1c54a83f00d276693d4f5
Marcin Marciniuk <marcinm222@gmail.com>
2023-03-27 21:19:36 +0200

    fix import

--- utils/pageList.ts
+++ utils/pageList.ts
@@ -1,16 +1,14 @@
+import MenuItem from "@/lib/types/MenuItem";
 import Menu from "@/models/Menu";
 
 export default async function pageList() {
-  const menu = await Menu.find({});
-
-  const filteredChildren = menu.filter(
-    (item) => item.custom === true && item.on === true
+  const filteredChildren: MenuItem[] = JSON.parse(
+    JSON.stringify(await Menu.find({ default: false, custom: true, on: true }))
   );
 
   return {
     nodes: filteredChildren.map((item) => {
       return {
-        id: item._id.toString(),
         slug: item.slug,
       };
     }),
